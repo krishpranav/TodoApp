0b6f4975fa808bef804f0d38a4426c6963e3c455
krishpranav <krisna.pranav@gmail.com>
2021-11-03 13:55:40 +0530

    TodoApp: improved ui with navigation controller
    updated the storyboard + swift view controller :)

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    private let table: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Todo List App"
+        view.addSubview(table)
+        table.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
 }
 
